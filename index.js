0f3ae524ef5229ae2de786ed7b997a8170c522a9
Manos <mkarapiperakis21@gmail.com>
2024-06-09 16:39:48 +0300

    add pop feature

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ import { LinkedList } from "./dataStructures/linkedList.js";
 function listInfo() {
   let myLinkedList = new LinkedList(4);
 
+  //push nodes
   //myLinkedList.clear();
   myLinkedList.push(1);
   myLinkedList.push(2);
+
+  //pop nodes
+  if (myLinkedList.length !== 0) console.log(myLinkedList.pop());
+  else console.log("null");
+
+  if (myLinkedList.length !== 0) console.log(myLinkedList.pop().value);
+  else console.log("null");
+
+  if (myLinkedList.length !== 0) console.log(myLinkedList.pop().value);
+  else console.log("null");
 
   myLinkedList.getHead();
   myLinkedList.getTail();
