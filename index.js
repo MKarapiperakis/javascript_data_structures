63b4e1333b5a966325b958b663eec54e9bc278d7
Manos <mkarapiperakis21@gmail.com>
2024-06-09 13:28:42 +0300

    add push feature

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import { LinkedList } from "./dataStructures/linkedList.js";
 function listInfo() {
   let myLinkedList = new LinkedList(4);
 
+  //myLinkedList.clear();
+  myLinkedList.push(1);
+  myLinkedList.push(2);
+
   myLinkedList.getHead();
   myLinkedList.getTail();
   myLinkedList.getLength();
