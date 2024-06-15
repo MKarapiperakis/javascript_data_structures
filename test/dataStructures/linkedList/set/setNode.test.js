b156fdd80103a17d1c5e013b9a68080a08b0495f
Manos <mkarapiperakis21@gmail.com>
2024-06-15 15:00:38 +0300

    add automated tests for set feature

---
+++ test/dataStructures/linkedList/set/setNode.test.js
@@ -0,0 +1,11 @@
+import { it, expect } from "vitest";
+import { LinkedList } from "../../../../dataStructures/linkedList";
+
+it("should push node", () => {
+  let myLinkedList = new LinkedList(2);
+  myLinkedList.push("test");
+
+  myLinkedList.set(1, 55);
+
+  expect(myLinkedList.get(1).value).toBe(55);
+});
