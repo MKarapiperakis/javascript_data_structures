7e7a73bdf126a1363dc80a9f816d97b3e6077921
Manos <mkarapiperakis21@gmail.com>
2024-06-24 14:11:35 +0300

    add enqueue feature for queues

--- dataStructures/queue.js
+++ dataStructures/queue.js
@@ -40,4 +40,16 @@ export class Queue {
     this.last = null;
     this.length = 0;
   }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+  }
 }
