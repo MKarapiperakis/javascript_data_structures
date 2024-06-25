39406a77ab847b6cccbaae1a4ae09efdb3dacd11
Manos <mkarapiperakis21@gmail.com>
2024-06-25 17:57:03 +0300

    add tree constructor

---
+++ dataStructures/tree.js
@@ -0,0 +1,13 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export class BST {
+  constructor() {
+    this.root = null;
+  }
+}
