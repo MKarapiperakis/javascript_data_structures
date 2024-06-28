ef3aea636cb1350bea40bebe349a5413ba3680ea
Manos <mkarapiperakis21@gmail.com>
2024-06-28 18:00:52 +0300

    add automated test for insert feature in BST

---
+++ test/dataStructures/tree/insert/insertNode.test.js
@@ -0,0 +1,40 @@
+import { it, describe, expect } from "vitest";
+import { BST } from "../../../../dataStructures/tree";
+
+describe.sequential("insert scenarios", () => {
+  it("should insert new node", () => {
+    let tree = new BST();
+
+    tree.insert(5);
+
+    expect(tree.root.value).toBe(5);
+  });
+
+  it("should insert new node", () => {
+    let tree = new BST();
+
+    tree.insert(5);
+    tree.insert(7);
+
+    expect(tree.root.right.value).toBe(7);
+  });
+
+  it("should insert new node", () => {
+    let tree = new BST();
+
+    tree.insert(5);
+    tree.insert(7);
+    tree.insert(3);
+
+    expect(tree.root.left.value).toBe(3);
+  });
+
+  it("should insert new node", () => {
+    let tree = new BST();
+
+    tree.insert(5);
+    tree.insert(5);
+
+    expect(tree.root.value).toBe(5);
+  });
+});
