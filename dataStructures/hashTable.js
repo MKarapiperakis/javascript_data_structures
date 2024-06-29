063c61757389769555ebc284b9f437951d46d0dc
Manos Karapiperakis <mkarapiperakis21@gmail.com>
2024-06-29 22:26:35 +0300

    HT  get feature (#24)

--- dataStructures/hashTable.js
+++ dataStructures/hashTable.js
@@ -24,4 +24,16 @@ export class HashTable {
     this.dataMap[index].push([key, value]);
     return this;
   }
+
+  get(key) {
+    let index = this._hash(key);
+    if (this.dataMap[index]) {
+      for (let i = 0; i < this.dataMap[index].length; i++) {
+        if (this.dataMap[index][i][0] === key) {
+          return this.dataMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
 }
